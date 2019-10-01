090ccb1c59d29c873d6bb9119f26db9d6f6b5a2a
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-10-01 13:16:34 +0300

    Abbreviate the types

--- library/Fx.hs
+++ library/Fx.hs
@@ -3,10 +3,10 @@ module Fx
   -- * Eio
   provideAndAccess,
   -- * Provider
-  Provider,
+  Pdr,
   acquireAndRelease,
   -- * Accessor
-  Accessor,
+  Asr,
   use,
   mapEnv,
   fork,
@@ -24,8 +24,8 @@ import qualified Exceptionless as Eio
 Having an environment provider, execute an action,
 which uses the environment and produces either an error or result.
 -}
-provideAndAccess :: Provider err env -> Accessor env err res -> Eio err res
-provideAndAccess (Provider providerEio) (Accessor accessorRdr) = do
+provideAndAccess :: Pdr err env -> Asr env err res -> Eio err res
+provideAndAccess (Pdr providerEio) (Asr accessorRdr) = do
   (env, release) <- providerEio
   Eio.bindErr
     (\ err -> release *> throwError err)
@@ -36,6 +36,7 @@ provideAndAccess (Provider providerEio) (Accessor accessorRdr) = do
 -------------------------
 
 {-|
+Provider.
 Effectful computation with explicit errors,
 which encompasses environment acquisition and releasing.
 
@@ -43,33 +44,33 @@ Composes well, allowing you to merge multiple providers into one.
 
 Builds up on some ideas expressed in http://www.haskellforall.com/2013/06/the-resource-applicative.html
 -}
-newtype Provider err env = Provider (Eio err (env, Eio err ()))
+newtype Pdr err env = Pdr (Eio err (env, Eio err ()))
 
-instance Functor (Provider err) where
-  fmap f (Provider m) = Provider $ do
+instance Functor (Pdr err) where
+  fmap f (Pdr m) = Pdr $ do
     (env, release) <- m
     return (f env, release)
 
-instance Applicative (Provider err) where
-  pure env = Provider (pure (env, pure ()))
-  Provider m1 <*> Provider m2 = Provider $
+instance Applicative (Pdr err) where
+  pure env = Pdr (pure (env, pure ()))
+  Pdr m1 <*> Pdr m2 = Pdr $
     liftA2 (\ (env1, release1) (env2, release2) -> (env1 env2, release2 *> release1)) m1 m2
 
-instance Monad (Provider err) where
+instance Monad (Pdr err) where
   return = pure
-  (>>=) (Provider m1) k2 = Provider $ do
+  (>>=) (Pdr m1) k2 = Pdr $ do
     (env1, release1) <- m1
-    (env2, release2) <- case k2 env1 of Provider m2 -> m2
+    (env2, release2) <- case k2 env1 of Pdr m2 -> m2
     return (env2, release2 >> release1)
 
-instance EioLifting err (Provider err) where
-  liftEio = Provider . fmap (\ a -> (a, return ()))
+instance EioLifting err (Pdr err) where
+  liftEio = Pdr . fmap (\ a -> (a, return ()))
 
-instance MonadIO (Provider SomeException) where
+instance MonadIO (Pdr SomeException) where
   liftIO = liftEio @SomeException . liftIO
 
-instance Bifunctor Provider where
-  bimap fn1 fn2 (Provider m) = Provider (bimap fn1 (fn2 *** first fn1) m)
+instance Bifunctor Pdr where
+  bimap fn1 fn2 (Pdr m) = Pdr (bimap fn1 (fn2 *** first fn1) m)
   second = fmap
 
 {-|
@@ -77,43 +78,44 @@ Create a resource provider from actions that don't fail.
 You can turn your exception-throwing actions into these
 by means of the `Eio` API.
 -}
-acquireAndRelease :: Eio err env -> (env -> Eio err ()) -> Provider err env
-acquireAndRelease acquire release = Provider (fmap (\ env -> (env, release env)) acquire)
+acquireAndRelease :: Eio err env -> (env -> Eio err ()) -> Pdr err env
+acquireAndRelease acquire release = Pdr (fmap (\ env -> (env, release env)) acquire)
 
 
 -- * Accessor
 -------------------------
 
 {-|
+Accessor.
 Effectful computation with explicit errors in context of provided environment.
 -}
-newtype Accessor env err res = Accessor (ReaderT env (Eio err) res)
+newtype Asr env err res = Asr (ReaderT env (Eio err) res)
 
-deriving instance Functor (Accessor env err)
-deriving instance Applicative (Accessor env err)
-deriving instance Monoid err => Alternative (Accessor env err)
-deriving instance Monad (Accessor env err)
-deriving instance Monoid err => MonadPlus (Accessor env err)
-deriving instance MonadError err (Accessor env err)
+deriving instance Functor (Asr env err)
+deriving instance Applicative (Asr env err)
+deriving instance Monoid err => Alternative (Asr env err)
+deriving instance Monad (Asr env err)
+deriving instance Monoid err => MonadPlus (Asr env err)
+deriving instance MonadError err (Asr env err)
 
-instance Bifunctor (Accessor env) where
-  first = mapAccessor . mapReaderT . first
+instance Bifunctor (Asr env) where
+  first = mapAsr . mapReaderT . first
   second = fmap
 
-instance EioLifting err (Accessor env err) where
-  liftEio = Accessor . lift
+instance EioLifting err (Asr env err) where
+  liftEio = Asr . lift
 
-instance MonadIO (Accessor env SomeException) where
-  liftIO = Accessor . liftIO
+instance MonadIO (Asr env SomeException) where
+  liftIO = Asr . liftIO
 
-mapAccessor :: (ReaderT envA (Eio errA) resA -> ReaderT envB (Eio errB) resB) -> Accessor envA errA resA -> Accessor envB errB resB
-mapAccessor mapper (Accessor impl) = Accessor (mapper impl)
+mapAsr :: (ReaderT envA (Eio errA) resA -> ReaderT envB (Eio errB) resB) -> Asr envA errA resA -> Asr envB errB resB
+mapAsr mapper (Asr impl) = Asr (mapper impl)
 
 {-|
 Map the environment of an accessor.
 -}
-mapEnv :: (b -> a) -> Accessor a err res -> Accessor b err res
-mapEnv fn = mapAccessor (withReaderT fn)
+mapEnv :: (b -> a) -> Asr a err res -> Asr b err res
+mapEnv fn = mapAsr (withReaderT fn)
 
 {-|
 Lift an env-using function into accessor.
@@ -125,16 +127,16 @@ This function leaks abstraction.
 It is your responsibility to make sure that you don't use the provided @env@
 outside of accessor.
 -}
-use :: (env -> Eio err a) -> Accessor env err a
-use = Accessor . ReaderT
+use :: (env -> Eio err a) -> Asr env err a
+use = Asr . ReaderT
 
 {-|
 Fork a computation to be run on a separate thread,
 blocking until it finishes or fails,
 while also performing a computation on current thread.
 -}
-fork :: Accessor env err () -> Accessor env err res -> Accessor env err res
-fork (Accessor fork) (Accessor main) = Accessor $ ReaderT $ \ env -> do
+fork :: Asr env err () -> Asr env err res -> Asr env err res
+fork (Asr fork) (Asr main) = Asr $ ReaderT $ \ env -> do
   blockVar <- Eio.liftSafeIO newEmptyMVar
   Eio.fork $ let
     handler err = Eio.liftSafeIO (putMVar blockVar (Just err))
