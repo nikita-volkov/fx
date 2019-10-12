63869165422395880e1777c72bef1113ab119706
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-10-12 21:31:28 +0300

    Test using "fail"

--- test/Main.hs
+++ test/Main.hs
@@ -37,4 +37,25 @@ main = defaultMain $ testGroup "" $
         Left (FxException _ (ErrorCallFxExceptionReason _)) -> return ()
         Left exc -> assertFailure (show exc)
         _ -> assertFailure "Right"
+    ,
+    testException "Fail"
+      (fail "A")
+      (\ case
+        FxException _ (ErrorCallFxExceptionReason _) -> True
+        exc -> False)
+    ,
+    testException "Fail on forked thread"
+      (start (fail "A") >>= wait)
+      (\ case
+        FxException _ (ErrorCallFxExceptionReason _) -> True
+        exc -> False)
   ]
+
+testException :: TestName -> Fx () Void a -> (FxException -> Bool) -> TestTree
+testException name fx validateExc = testCase name $ do
+  res <- try @FxException $ runFx $ fx
+  case res of
+    Left exc -> if validateExc exc
+      then return ()
+      else assertFailure (show exc)
+    Right _ -> assertFailure "No exception"
