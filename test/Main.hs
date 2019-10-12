c49482749e82d2c5d268124972e06e0b3e25a216
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-10-12 20:04:28 +0300

    Add tests on error call

--- test/Main.hs
+++ test/Main.hs
@@ -13,4 +13,28 @@ import qualified Test.QuickCheck.Property as QuickCheck
 
 main = defaultMain $ testGroup "" $
   [
+    testCase "Error call on main thread" $ do
+      res <- try $ runFx $ error "A"
+      case res of
+        Left (FxException _ (ErrorCallFxExceptionReason _)) -> return ()
+        Left exc -> assertFailure (show exc)
+        _ -> assertFailure "Right"
+    ,
+    testCase "Error call on forked thread" $ do
+      res <- try $ runFx $ do
+        future <- start $ error "A"
+        wait future
+      case res of
+        Left (FxException _ (ErrorCallFxExceptionReason _)) -> return ()
+        Left exc -> assertFailure (show exc)
+        _ -> assertFailure "Right"
+    ,
+    testCase "Error call on deeply forked thread" $ do
+      res <- try $ runFx $ do
+        future <- start $ start $ error "A"
+        wait future >>= wait
+      case res of
+        Left (FxException _ (ErrorCallFxExceptionReason _)) -> return ()
+        Left exc -> assertFailure (show exc)
+        _ -> assertFailure "Right"
   ]
