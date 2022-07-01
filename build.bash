55e4dca3fd3986307ff1e3b2e34ef945a5769967
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-07-01 15:47:51 +0300

    Update the build script

--- build.bash
+++ build.bash
@@ -4,23 +4,34 @@ set -eo pipefail
 function format {
   ormolu --mode inplace -ce \
   $(find . -name "*.hs" \
+    -not -path "./.git/*" \
     -not -path "./*.stack-work/*" \
-    -not -path "./.git/*")
+    -not -path "./samples/*" \
+    -not -path "./sketches/*" \
+    -not -path "./output/*" \
+    -not -path "./ideas/*" \
+    -not -path "./refs/*" \
+    -not -path "./temp/*")
 }
 
 function build_and_test {
   stack build \
-  --ghc-options "-j +RTS -A128m -n2m -RTS -fwarn-incomplete-patterns" \
-  --test \
-  --fast
+  --fast --test \
+  --ghc-options "-j +RTS -A128m -n2m -RTS -fwarn-incomplete-patterns"
 }
 
-function build_and_test_with_doctest {
+function build_and_run_test_suite {
   stack build \
+  --fast --test \
   --ghc-options "-j +RTS -A128m -n2m -RTS -fwarn-incomplete-patterns" \
-  --test \
-  --fast \
-  --flag coalmine:doctest
+  :$1
+}
+
+function build_and_test_by_pattern {
+  stack build \
+  --fast --test \
+  --ghc-options "-j +RTS -A128m -n2m -RTS -fwarn-incomplete-patterns" \
+  --ta "-p \"$1\"" $2
 }
 
 function build {
@@ -29,8 +40,33 @@ function build {
   --fast
 }
 
-function demo {
-  stack exec demo
+function build_haddock {
+  stack haddock \
+  --ghc-options "-j +RTS -A128m -n2m -RTS -fwarn-incomplete-patterns" \
+  --fast
+}
+
+function build_failing_on_incomplete_patterns {
+  stack build \
+  --ghc-options "-j +RTS -A128m -n2m -RTS -Werror=incomplete-patterns" \
+  --fast
+}
+
+function install {
+  stack \
+  --work-dir ".install.stack-work" \
+  install \
+  --ghc-options "-j +RTS -A128m -n2m -RTS"
+}
+
+function install_forking {
+  mkdir -p .build-logs
+  install > .build-logs/install.stdout 2> .build-logs/install.stderr &
+}
+
+function build_haddock_forking {
+  mkdir -p .build-logs
+  build_haddock > .build-logs/build_haddock.stdout 2> .build-logs/build_haddock.stderr &
 }
 
 format
