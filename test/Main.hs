bdf86a25f175ec8374aa9a8e633fc019fa6189b2
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-05-04 16:17:48 +0300

    Modernize

--- test/Main.hs
+++ test/Main.hs
@@ -1,27 +1,24 @@
 module Main where
 
 import Fx
-import qualified Test.QuickCheck as QuickCheck
-import Test.QuickCheck.Instances
-import qualified Test.QuickCheck.Property as QuickCheck
 import Test.Tasty
 import Test.Tasty.HUnit
-import Test.Tasty.QuickCheck
-import Test.Tasty.Runners
 import Prelude hiding (choose)
 
+main :: IO ()
 main =
-  defaultMain $
-    testGroup "" $
-      [ testCase "Error call on main thread" $ do
+  defaultMain
+    $ testGroup ""
+    $ [ testCase "Error call on main thread" $ do
           res <- try $ runFx $ error "A"
           case res of
             Left (FxException _ (ErrorCallFxExceptionReason _)) -> return ()
             Left exc -> assertFailure (show exc)
             _ -> assertFailure "Right",
         testCase "Error call on forked thread" $ do
-          res <- try $
-            runFx $ do
+          res <- try
+            $ runFx
+            $ do
               future <- start $ error "A"
               wait future
           case res of
@@ -29,8 +26,9 @@ main =
             Left exc -> assertFailure (show exc)
             _ -> assertFailure "Right",
         testCase "Error call on deeply forked thread" $ do
-          res <- try $
-            runFx $ do
+          res <- try
+            $ runFx
+            $ do
               future <- start $ start $ error "A"
               wait future >>= wait
           case res of
@@ -42,14 +40,14 @@ main =
           (fail "A")
           ( \case
               FxException _ (ErrorCallFxExceptionReason _) -> True
-              exc -> False
+              _ -> False
           ),
         testException
           "Fail on forked thread"
           (start (fail "A") >>= wait)
           ( \case
               FxException _ (ErrorCallFxExceptionReason _) -> True
-              exc -> False
+              _ -> False
           )
       ]
 
