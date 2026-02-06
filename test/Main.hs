a235f2b1c7e27ca65578850b51782289e2fc52a0
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-06 20:49:00 +0300

    Migrate the tests to hspec

--- test/Main.hs
+++ test/Main.hs
@@ -1,35 +1,32 @@
 module Main where
 
 import Fx
-import Test.Tasty
-import Test.Tasty.HUnit
+import Test.Hspec
 import Prelude hiding (choose)
 
 main :: IO ()
-main =
-  defaultMain
-    $ testGroup ""
-    $ [ testCase "Error call on main thread" $ do
-          res <- try $ runFx $ error "A"
-          case res of
-            Left (FxException _ (ErrorCallFxExceptionReason _)) -> return ()
-            Left exc -> assertFailure (show exc)
-            _ -> assertFailure "Right",
-        testException
-          "Fail"
-          (fail "A")
-          ( \case
-              FxException _ (ErrorCallFxExceptionReason _) -> True
-              _ -> False
-          )
-      ]
+main = hspec do
+  it "Error call on main thread" do
+    res <- try $ runFx $ error "A"
+    case res of
+      Left (FxException _ (ErrorCallFxExceptionReason _)) -> return ()
+      Left exc -> expectationFailure (show exc)
+      _ -> expectationFailure "Right"
 
-testException :: TestName -> Fx () Void a -> (FxException -> Bool) -> TestTree
-testException name fx validateExc = testCase name $ do
+  it "Fail" do
+    testException
+      (fail "A")
+      ( \case
+          FxException _ (ErrorCallFxExceptionReason _) -> True
+          _ -> False
+      )
+
+testException :: Fx () Void a -> (FxException -> Bool) -> IO ()
+testException fx validateExc = do
   res <- try @FxException $ runFx $ fx
   case res of
     Left exc ->
       if validateExc exc
         then return ()
-        else assertFailure (show exc)
-    Right _ -> assertFailure "No exception"
+        else expectationFailure (show exc)
+    Right _ -> expectationFailure "No exception"
